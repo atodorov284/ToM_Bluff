545974083e850828758f43ba5689a2acd44ee774
03chrisk <chris.kobriger@gmail.com>
2024-12-17 18:52:39 +0100

    adding an outline for first order and writing thoughts in docstrings

--- bluff-game/agents/first_order.py
+++ bluff-game/agents/first_order.py
@@ -22,13 +22,25 @@ class FirstOrderAgent(BaseAgent):
 
     def select_action(self, observation: dict, mask: list):
         """
-                If else statemment.
-                If you believe opponent is bluffing, then challenge,
-
-                Else figure out whether opponent will challenge you and play bluff ofr truthfully accordingly.
-
+        FOR DECIDING WHETHER TO CHALLENGE OR NOT
+                If you believe opponent is bluffing, then increase the q value for challenging them.
+                if they dont play the number of cards you believe, then fall back to using the q table to figure out if you should challenge them.
+                Else figure out whether opponent will challenge you and play bluff or truthfully accordingly.
+                
+        FOR DECIDING WHETHER TO BLUFF OR PLAY TRUTHFULLY
+                Do it iteratively. Simulate possible actions you can make and using the model estimate what the opponent would do. An action is selected if it is valid.
+                Valid actions in this case are for example if you simulate playing bluff, and you think your opponent would not challenge you, or if you played truthfully 
+                and you think your opponent would challenge you. Essentially whenever you would trick your opponent you have a valid action and the action you actually play is 
+                the valid action that you estimate to bring you the most reward.        
+                
         """
 
+        pass
+
+    def _estimate_opponent_state(self, observation: dict) -> list:
+        """
+        reset estimate of opponent state whenever we play challenge or central pile size is 0, as then we know exactly what the opponent is holding. 
+        """
         pass
 
     def update(self, reward: float, next_observation: dict = None) -> None:
@@ -38,4 +50,14 @@ class FirstOrderAgent(BaseAgent):
         pass
 
     def _select_opponent_action(self, observation: dict, mask: list) -> list:
-        pass
+        """Predict opponent's next action using the opponent model."""
+        # Create opponent observation using our estimates
+        opponent_obs = {
+            "current_rank": observation["current_rank"],
+            "central_pile_size": observation["central_pile_size"],
+            "hand": None, # TODO implement this!!
+            "cards_other_agent_played": sum(observation["hand"])
+        }
+        
+        return self.opponent_model.select_action(opponent_obs, mask)
+
